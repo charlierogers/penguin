8d8d9b9b1839c546673dceaaa7114f78a0c525fb
Charlie Rogers <charlierogers3.14@gmail.com>
2018-02-04 13:44:28 -0500

    added ball

---
+++ Assets/Entities/Ball/Scripts/BallMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallMovement : MonoBehaviour {
+
+    public float movement_speed = 1.0f;
+    public float bounce_force = 50.0f;
+    public float anti_gravity = 3.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = new Vector3(movement_speed, rb.velocity.y, rb.velocity.z);
+        if (rb.velocity.y < 0.0f) {
+            rb.AddForce(Vector3.up * anti_gravity);
+        }
+
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("collision: " + LayerMask.LayerToName(collision.gameObject.layer));
+        if (collision.gameObject.layer == LayerMask.NameToLayer("FloorTile")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } else if (collision.gameObject.layer == LayerMask.NameToLayer("Penguin")) {
+            rb.AddForce(Vector3.up * bounce_force);
+            Debug.Log("bounce");
+        }
+    }
+}
---
+++ Assets/Shared Scripts/VelocityLimit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityLimit : MonoBehaviour {
+
+    public float vertical_speed_limit = 7.0f;
+    public float horizontal_speed_limit = 4.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = new Vector3(Mathf.Min(rb.velocity.x, horizontal_speed_limit),
+            Mathf.Min(rb.velocity.y, vertical_speed_limit), rb.velocity.z);
+	}
+}
